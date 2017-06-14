a68dcdc2eef7d0179897dffdedc602c9dbd00fa2
Hiraku Ohno <hiraku.ohno@glucose.jp>
2017-06-14 19:14:09 +0900

    Add initial_tasks.sh

---
+++ initial_tasks.sh
@@ -0,0 +1,13 @@
+sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install rbenv
+brew install nodebrew
+nodebrew install-binary latest
+
+npm install -g npmbrew
+
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+sudo pip install virtualfish
+
