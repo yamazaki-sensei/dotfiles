83939a2139258c6efa39623172919ab08c17963e
Hiraku Ohno <hiraku.ohno@glucose.jp>
2017-06-14 18:51:32 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+ln -s ~/.bashrc .bashrc
+ln -s ~/.gitconfig .gitconfig
+ln -s ~/.gitignore .gitignore
+ln -s ~/.gvimrc .gvimrc
+ln -s ~/.ideavimrc .ideavimrc
+ln -s ~/.inputrc .inputrc
+ln -s ~/.vim .vim
+ln -s ~/.vimrc .vimrc
+ln -s ~/.xvimrc .xvimrc
+ln -s ~/.zshrc .zshrc
+ln -s ~/.config/fish/config.fish config.fish
+
