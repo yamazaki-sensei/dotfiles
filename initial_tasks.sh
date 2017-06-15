74f5b457845ad6a48add10109bf0ac2157e04ccb
Hiraku Ohno <hiraku.ohno@glucose.jp>
2017-06-15 11:14:54 +0900

    Update initial tasks

--- initial_tasks.sh
+++ initial_tasks.sh
@@ -1,5 +1,8 @@
 sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install fish
+brew install git
+brew install hub
 brew update
 brew install rbenv
 brew install nodebrew
@@ -10,4 +13,7 @@ npm install -g npmbrew
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 sudo pip install virtualfish
+
+sudo echo '/usr/local/bin/fish' >> /etc/shells
+chsh -s /usr/local/bin/fish
 
