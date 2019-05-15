5418e4f6e740475a6f6123d1bb6c52339f837ac5
Hiraku Ohno <hiraku.ohno@integrity-healthcare.co.jp>
2019-05-15 15:05:03 +0900

    Update scripts

--- initial_tasks.sh
+++ initial_tasks.sh
@@ -1,18 +1,18 @@
-sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
 brew install fish
 brew install git
 brew install hub
-brew update
 brew install rbenv
 brew install nodebrew
 nodebrew install-binary latest
 
-npm install -g npmbrew
+#npm install -g npmbrew
 
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
-sudo pip install virtualfish
+#sudo pip install virtualenv
+#sudo pip install virtualenvwrapper
+#sudo pip install virtualfish
 
 sudo echo '/usr/local/bin/fish' >> /etc/shells
 chsh -s /usr/local/bin/fish
--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-ln -s .bashrc ~
-ln -s .gitconfig ~
-ln -s .gitignore ~
-ln -s .gvimrc ~
-ln -s .ideavimrc ~
-ln -s .inputrc ~
-ln -s .vim ~
-ln -s .vimrc ~
-ln -s .xvimrc ~
-ln -s .zshrc ~
+rm -f ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+rm -f ~/.gitignore
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+rm -f ~/.gvimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+rm -f ~/.ideavimrc
+ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
+rm -f ~/.inputrc
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+rm -fr ~/.vim
+ln -s ~/dotfiles/.vim ~/.vim
+rm -f ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+rm -f ~/.xvimrc
+ln -s ~/dotfiles/.xvimrc ~/.xvimrc
 
