946eebe0a7a6fc3fdca3206e83c8d2aa90069bc1
hira <ohno@thelooper.jp>
2023-06-27 12:00:37 +0900

    恐らくもう使わないであろう設定を削除

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,8 @@
 # alias
 alias rm='rm -i'
 alias be='bundle exec'
-alias refresh_db='be rails db:drop; be rails db:create; be rails db:apply; be rails db:seed'
 set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
 alias g='git'
-alias clearcache='rm -fr ~/Library/Developer/Xcode/DerivedData/*; rm -fr ~/Library/Caches/org.carthage.CarthageKit/; rm -fr ~/Library/Caches/carthage/'
 alias killapps='launchctl reboot aps'
 alias dcm='docker compose'
 
@@ -14,10 +12,6 @@ end
 
 # PATH
 set PATH $HOME/.nodebrew/current/bin $PATH
-set PATH $HOME/Library/Android/sdk/platform-tools $PATH
-set PATH $HOME/go/bin $PATH
-set PATH /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home/bin $PATH
-set JAVA_HOME /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home
 
 set -U fish_prompt_pwd_dir_length 0
 
